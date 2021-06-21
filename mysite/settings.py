6a0a95c417aabbf1b2fa343bbcb146a45a407870
Kadzima <pascal.german@yandex.ru>
2021-06-21 13:38:36 +0600

    somde update settubgs file

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS','*', cast=Csv())
 
 
 # Application definition
